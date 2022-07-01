532a58cc4dfd560610cc569b2bbecc2750e4b43a
chenchunfeng09 <1005525953@qq.com>
2022-07-01 10:23:42 +0800

    feat(package.json): 添加commitizen

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // function 跟括号之间的空格
+    'space-before-function-paren': 'off'
   }
 }
